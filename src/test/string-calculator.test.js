aa2e3950bba72329ce32c9bbe71fa18c6c988c16
rishab <rishab@192.168.1.4>
2024-10-05 18:31:02 +0530

    added failing test for n comma separated inputs

--- src/test/string-calculator.test.js
+++ src/test/string-calculator.test.js
@@ -5,18 +5,18 @@ import { calculator } from "../main/string-calculator.js";
 describe("add - basic functionality", () => {
   // claims or assertions we want to make about our code
   //claims should back up the assertion we made in this functionality written in the string
-  checkResult("",0);
-  checkResult("1",1)
+  checkResult("", 0);
+  checkResult("1", 1);
 });
 
 describe("add - with comma separator", () => {
-  checkResult("1,5",6);
+  checkResult("1,5", 6);
+  checkResult("1,2,3,4,5", 15); //added test to handle any amount of numbers
 });
 
-function checkResult(expression, expected){
-  it('should return '+ expected  + ' when passed "'+expression +'"' , () => {
-     const actual_result = calculator.add(expression);
+function checkResult(expression, expected) {
+  it("should return " + expected + ' when passed "' + expression + '"', () => {
+    const actual_result = calculator.add(expression);
     expect(actual_result).to.equal(expected);
   });
-
 }
