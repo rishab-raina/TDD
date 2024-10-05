704a97428c219837a308462ae40279f432bec1ce
rishab <rishab@192.168.1.4>
2024-10-05 19:26:03 +0530

    added code to pass new line separated input numbers

--- src/main/string-calculator.js
+++ src/main/string-calculator.js
@@ -1,8 +1,8 @@
-//string calculator add method with n comma separated numbers as input
+//string calculator add method with n comma and new-line(\n) separated numbers as input
 
 export const calculator = {
   add: (expression) => {
-    let numbers = expression.split(",");
+    let numbers = expression.split(/,|\n/g);
     let sum = 0;
     for (let index = 0; index < numbers.length; index++) {
       sum += parseInt(numbers[index] || 0);
